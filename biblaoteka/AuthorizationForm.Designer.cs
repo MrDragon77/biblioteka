13ee0dd905b7e33309bb6b14eb869ed56be5216e
MrDragon77 <azat.yag@gmail.com>
2024-04-17 15:24:43 +0500

    start

---
+++ biblaoteka/AuthorizationForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace biblaoteka
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            UserNameTextBox = new TextBox();
+            PasswordTextBox = new TextBox();
+            LoginButton = new Button();
+            SuspendLayout();
+            // 
+            // UserNameTextBox
+            // 
+            UserNameTextBox.Location = new Point(128, 73);
+            UserNameTextBox.Name = "UserNameTextBox";
+            UserNameTextBox.Size = new Size(100, 23);
+            UserNameTextBox.TabIndex = 0;
+            // 
+            // PasswordTextBox
+            // 
+            PasswordTextBox.Location = new Point(128, 140);
+            PasswordTextBox.Name = "PasswordTextBox";
+            PasswordTextBox.Size = new Size(100, 23);
+            PasswordTextBox.TabIndex = 1;
+            // 
+            // LoginButton
+            // 
+            LoginButton.Location = new Point(136, 201);
+            LoginButton.Name = "LoginButton";
+            LoginButton.Size = new Size(75, 23);
+            LoginButton.TabIndex = 2;
+            LoginButton.Text = "button1";
+            LoginButton.UseVisualStyleBackColor = true;
+            LoginButton.Click += LoginButton_Click;
+            // 
+            // AuthorizationForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(LoginButton);
+            Controls.Add(PasswordTextBox);
+            Controls.Add(UserNameTextBox);
+            Name = "AuthorizationForm";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox UserNameTextBox;
+        private TextBox PasswordTextBox;
+        private Button LoginButton;
+    }
+}
---
+++ biblaoteka/AuthorizationForm.cs
@@ -0,0 +1,15 @@
+namespace biblaoteka
+{
+    public partial class AuthorizationForm : Form
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
