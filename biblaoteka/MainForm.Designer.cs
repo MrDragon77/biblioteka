92086aed909758594a51294cf16dcfb7b78730a5
Kamil Batyrshin <pepeluganich@gmail.com>
2024-04-26 13:02:53 +0500

    Bruh

--- biblaoteka/MainForm.Designer.cs
+++ biblaoteka/MainForm.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // BooksButton
             // 
-            BooksButton.Location = new Point(98, 137);
+            BooksButton.Location = new Point(112, 183);
+            BooksButton.Margin = new Padding(3, 4, 3, 4);
             BooksButton.Name = "BooksButton";
-            BooksButton.Size = new Size(94, 50);
+            BooksButton.Size = new Size(107, 67);
             BooksButton.TabIndex = 0;
             BooksButton.Text = "Books";
             BooksButton.UseVisualStyleBackColor = true;
@@ -46,9 +47,10 @@
             // 
             // AddBookButton
             // 
-            AddBookButton.Location = new Point(271, 137);
+            AddBookButton.Location = new Point(310, 183);
+            AddBookButton.Margin = new Padding(3, 4, 3, 4);
             AddBookButton.Name = "AddBookButton";
-            AddBookButton.Size = new Size(94, 50);
+            AddBookButton.Size = new Size(107, 67);
             AddBookButton.TabIndex = 1;
             AddBookButton.Text = "Add book";
             AddBookButton.UseVisualStyleBackColor = true;
@@ -56,9 +58,10 @@
             // 
             // ReadersButton
             // 
-            ReadersButton.Location = new Point(456, 137);
+            ReadersButton.Location = new Point(521, 183);
+            ReadersButton.Margin = new Padding(3, 4, 3, 4);
             ReadersButton.Name = "ReadersButton";
-            ReadersButton.Size = new Size(94, 50);
+            ReadersButton.Size = new Size(107, 67);
             ReadersButton.TabIndex = 2;
             ReadersButton.Text = "Readers";
             ReadersButton.UseVisualStyleBackColor = true;
@@ -66,9 +69,10 @@
             // 
             // LogOutButton
             // 
-            LogOutButton.Location = new Point(21, 16);
+            LogOutButton.Location = new Point(325, 198);
+            LogOutButton.Margin = new Padding(3, 4, 3, 4);
             LogOutButton.Name = "LogOutButton";
-            LogOutButton.Size = new Size(68, 27);
+            LogOutButton.Size = new Size(78, 36);
             LogOutButton.TabIndex = 3;
             LogOutButton.Text = "Log out";
             LogOutButton.UseVisualStyleBackColor = true;
@@ -76,13 +80,14 @@
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(LogOutButton);
             Controls.Add(ReadersButton);
             Controls.Add(AddBookButton);
             Controls.Add(BooksButton);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "MainForm";
             Text = "MainForm";
             ResumeLayout(false);
